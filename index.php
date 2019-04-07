81ce5732fc47a04afbea8e8d077cf0ab67fb0852
yukorengan <fauzijuventini@gmail.com>
2019-04-08 07:30:29 +1400

    ganti dengan css cdn

--- index.php
+++ index.php
@@ -3,31 +3,11 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no">
-    <link rel="stylesheet" href="css/metro-all.min.css">
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+    <link href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Pinjaman Online Terpercaya Hehe</title>
 </head>
 <body>
-    <div class="container" style="margin-top:20px">
-        <!-- content here -->
-        <form>
-            <div class="form-group">
-                <label>Email address</label>
-                <input type="email" placeholder="Enter email"/>
-                <small class="text-muted">We'll never share your email with anyone else.</small>
-            </div>
-            <div class="form-group">
-                <label>Password</label>
-                <input type="password" placeholder="Enter email"/>
-            </div>
-            <div class="form-group">
-                <input type="checkbox" data-role="checkbox" data-caption="Remember me">
-            </div>
-            <div class="form-group">
-                <button class="button success">Submit data</button>
-                <input type="button" class="button" value="Cancel">
-            </div>
-        </form>
-    </div>
 </body>
 </html>
