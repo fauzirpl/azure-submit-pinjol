db9995465f4991f3ccbb16ed228a3f298ea0dfda
yukorengan <fauzijuventini@gmail.com>
2019-04-08 07:02:40 +1400

    Perubahan css

--- index.php
+++ index.php
@@ -2,11 +2,32 @@
 <html lang="en">
 <head>
     <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no">
+    <link rel="stylesheet" href="css/metro-all.min.css">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
+    <title>Pinjaman Online Terpercaya Hehe</title>
 </head>
 <body>
-    
+    <div class="container" style="margin-top:20px">
+        <!-- content here -->
+        <form>
+            <div class="form-group">
+                <label>Email address</label>
+                <input type="email" placeholder="Enter email"/>
+                <small class="text-muted">We'll never share your email with anyone else.</small>
+            </div>
+            <div class="form-group">
+                <label>Password</label>
+                <input type="password" placeholder="Enter email"/>
+            </div>
+            <div class="form-group">
+                <input type="checkbox" data-role="checkbox" data-caption="Remember me">
+            </div>
+            <div class="form-group">
+                <button class="button success">Submit data</button>
+                <input type="button" class="button" value="Cancel">
+            </div>
+        </form>
+    </div>
 </body>
 </html>
